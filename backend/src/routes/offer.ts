544571b176d5a6a5195f4d1694b14888507fa5e9
Languzda <langusda@gmail.com>
2024-04-17 12:49:03 +0200

    getActiveOffers give dummyData

--- backend/src/routes/offer.ts
+++ backend/src/routes/offer.ts
@@ -4,6 +4,18 @@ import express from "express";
 
 const router = express.Router();
 
+const dummyData = [
+    {name: "offer1", type: "type1", description: "description1", limit: 1, price: 5.99, photo: "photo1", card_id: 1, status: "active"},
+    {name: "offer2", type: "type2", description: "description2", limit: 2, price: 6.99, photo: "photo2", card_id: 2, status: "active"},
+    {name: "offer3", type: "type3", description: "description3", limit: 3, price: 7.99, photo: "photo3", card_id: 3, status: "active"},
+    {name: "offer4", type: "type4", description: "description4", limit: 4, price: 8.99, photo: "photo4", card_id: 4, status: "active"},
+    {name: "offer5", type: "type5", description: "description5", limit: 5, price: 9.99, photo: "photo5", card_id: 5, status: "active"},
+    {name: "offer6", type: "type6", description: "description6", limit: 6, price: 10.99, photo: "photo6", card_id: 6, status: "active"},
+    {name: "offer7", type: "type7", description: "description7", limit: 7, price: 11.99, photo: "photo7", card_id: 7, status: "active"},
+    {name: "offer8", type: "type8", description: "description8", limit: 8, price: 12.99, photo: "photo8", card_id: 8, status: "active"},
+    {name: "offer9", type: "type9", description: "description9", limit: 9, price: 13.99, photo: "photo9", card_id: 9, status: "active"},
+    {name: "offer10", type: "type10", description: "description10", limit: 10, price: 14.99, photo: "photo10", card_id: 10, status: "active"},
+]
 
 router.post("/addOffer", async (req: Request, res: Response) => {
     const prisma = new PrismaClient();
@@ -43,30 +55,30 @@ router.post("/addOffer", async (req: Request, res: Response) => {
 
 
 router.get("/getActiveOffers", async (req: Request, res: Response) => {
-    const prisma = new PrismaClient();
-
-    let offers
-
-    try {
-        offers = await prisma.offer.findMany({
-            where: {
-                status: "active"
-            }
-        });
-        res.statusCode = 200
-    } catch (e) {
-        console.error("ERROR:", e)
-        res.statusCode = 400
-    }
+    // const prisma = new PrismaClient();
+    //
+    // let offers
+    //
+    // try {
+    //     offers = await prisma.offer.findMany({
+    //         where: {
+    //             status: "active"
+    //         }
+    //     });
+    //     res.statusCode = 200
+    // } catch (e) {
+    //     console.error("ERROR:", e)
+    //     res.statusCode = 400
+    // }
 
     const responseDate = {
         message: "offers data",
         data: {
-            offers: offers
+            offers: dummyData
         },
     };
 
-    prisma.$disconnect();
+    // prisma.$disconnect();
 
     res.json(responseDate);
 });
