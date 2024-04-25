af95a412763c81767cec49a7d54b117693011beb
Languzda <langusda@gmail.com>
2024-04-25 19:30:44 +0200

    new requests to db

--- backend/src/controllers/dbControllers/offer.ts
+++ backend/src/controllers/dbControllers/offer.ts
@@ -1,4 +1,5 @@
 import { prisma } from '../prismaClient';
+import { Offer } from '@prisma/client';
 
 export function addOfferToDB(
   name: string,
@@ -25,6 +26,28 @@ export function addOfferToDB(
 }
 
 export function getActiveOffersFromDB() {
+  return prisma.offer.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      price: true,
+    },
+    where: {
+      status: 'active',
+    },
+  });
+}
+
+export function getOfferByIdFromDB(offer_id: Offer['id']) {
+  return prisma.offer.findUnique({
+    where: {
+      id: offer_id,
+    },
+  });
+}
+
+export function getActiveOffersAllFromDB() {
   return prisma.offer.findMany({
     where: {
       status: 'active',
