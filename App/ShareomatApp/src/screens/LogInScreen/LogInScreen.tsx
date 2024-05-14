5a8499c695b6f6b6de3aacf3156694ca6f47eda4
825a <137793260+825a@users.noreply.github.com>
2024-05-14 18:59:35 +0200

    add log in screen

---
+++ App/ShareomatApp/src/screens/LogInScreen/LogInScreen.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+import { Alert, Button, SafeAreaView, StatusBar, TextInput, useColorScheme } from "react-native"
+import { Colors } from "react-native/Libraries/NewAppScreen"
+import { styles } from "./Styles"
+
+function LogInScreen(): React.JSX.Element {
+
+  ////const ip = '172.27.112.1'
+  const ip = "192.168.1.102"
+  const port = '3000'
+
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  }
+
+  const [login, onChangeLogin] = React.useState('')
+  const [password, onChangePassword] = React.useState('')
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ login: login, password: password })
+  }
+
+  const onPressLogIn = async () => {
+    try {
+      await fetch(
+        `http://${ip}:${port}/user/login`, requestOptions)
+        .then(response => {
+          response.json()
+            .then(data => {
+              Alert.alert("Server response: ", data.message)
+            })
+        })
+    }
+    catch (e: any) {
+      console.error(e)
+    }
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeLogin}
+        placeholder="login"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        secureTextEntry={true}
+        placeholder="hasło"
+      />
+
+      <Button
+        onPress={onPressLogIn}
+        title="zaloguj"
+      />
+
+    </SafeAreaView>
+  )
+}
+
+export default LogInScreen
