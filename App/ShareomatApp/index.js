2ef07246f27b8f61f3ace95d2c15b398b7f48b1f
825a <137793260+825a@users.noreply.github.com>
2024-05-19 00:26:40 +0200

    add barcode screen

--- App/ShareomatApp/index.js
+++ App/ShareomatApp/index.js
@@ -4,7 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 //import App from './src/screens/OfferListScreen/OfferListScreen';
-import App from './src/screens/LogInScreen/LogInScreen';
+//import App from './src/screens/LogInScreen/LogInScreen';
+//import App from './src/screens/RegisterScreen/RegisterScreen';
+import App from './src/screens/BarcodeScreen/BarcodeScreen';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ App/ShareomatApp/src/screens/BarcodeScreen/Styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  codeText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
