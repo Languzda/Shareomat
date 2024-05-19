3f66f56ea13aff84b5eeac372daa5c1e1d7daff4
Languzda <langusda@gmail.com>
2024-05-19 20:35:28 +0200

    wrap app with authContext

--- App/ShareomatApp/src/App.tsx
+++ App/ShareomatApp/src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import LogInScreen from './screens/LogInScreen/LogInScreen';
 import AppScreen from './screens/AppScreen/AppScreen';
+import AuthContextProvider from './store/AuthContextProvider.tsx';
 
 function App(): React.JSX.Element {
-    const loggedIn = true;
+  const loggedIn = true;
 
-    if (loggedIn) {
-        return (<AppScreen />);
-    }
-    else {
-        return (<LogInScreen />);
-    }
+  if (loggedIn) {
+    return (
+      //@ts-ignore
+      <AuthContextProvider>
+        <AppScreen />
+      </AuthContextProvider>
+    );
+  } else {
+    return (
+      //@ts-ignore
+      <AuthContextProvider>
+        <LogInScreen />
+      </AuthContextProvider>
+    );
+  }
 }
 
 export default App;
